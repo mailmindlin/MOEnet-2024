89a7edabe62020b559e5ec5d563cf4d7f30dfd33
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-01-22 15:57:31 -0500

    Add tests for NT types

---
+++ server/nt_util/core_test.py
@@ -0,0 +1,50 @@
+from ntcore import NetworkTableInstance
+from unittest import TestCase
+
+class NtTestCase(TestCase):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.server = None
+        self.client = None
+
+    def setUp(self) -> None:
+
+        if self.server is None:
+            self.server = NetworkTableInstance.create()
+            self.server.startLocal()
+            self.server.startServer(listen_address='localhost', port4=5810)
+        if self.client is None:
+            self.client = self.server
+            # self.client = NetworkTableInstance.create()
+            # self.client.setServer('localhost', 5810)
+            # self.client.startClient4('client')
+        
+        assert self.client.isConnected()
+        
+        return super().setUp()
+    def tearDown(self) -> None:
+        if self.server is not None:
+            self.server.stopServer()
+            self.server = None
+        if self.client is not None:
+            self.client.stopClient()
+            self.client = None
+
+        return super().tearDown()
+
+class NtTestRelay(NtTestCase):
+    def test_relay_s2c(self):
+        with (
+            self.server.getIntegerTopic("test").publish() as pub,
+            self.client.getIntegerTopic("test").subscribe(1) as sub
+        ):
+            pub.set(5)
+            assert sub.get() == 5
+    
+    def test_relay_s2c(self):
+        with (
+            self.client.getIntegerTopic("test").publish() as pub,
+            self.server.getIntegerTopic("test").subscribe(1) as sub
+        ):
+            pub.set(5)
+            assert sub.get() == 5
---
+++ server/nt_util/protobuf_test.py
@@ -0,0 +1,29 @@
+import time
+from .core_test import NtTestCase
+from .protobuf import ProtobufPublisher, ProtobufSubscriber, ProtobufTopic
+try:
+	from ..typedef.net import Timestamp
+except ImportError:
+	import sys, os
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'typedef'))
+	from typedef.net import Timestamp
+
+
+class NtProtoTest(NtTestCase):
+	def test_relay(self):
+		default_value = Timestamp(seconds=123, nanos=456)
+		with ProtobufTopic.wrap(self.server, "test_proto", Timestamp) as topic:
+			with (
+				topic.publish() as pub,
+				topic.subscribe(default_value) as sub
+			):
+				ts_int = time.time_ns()
+				ts = Timestamp(
+					seconds=int(ts_int / 1_000_000_000),
+					nanos=int(ts_int % 1_000_000_000),
+				)
+				# Not published yet
+				assert sub.get() == default_value
+				assert sub.get("sdf") == "sdf"
+				pub.set(ts)
+				assert sub.get() == ts

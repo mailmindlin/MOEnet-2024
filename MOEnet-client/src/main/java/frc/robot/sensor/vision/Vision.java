6227f753c1c4eba093d7ad572e1f6607fff9da5d
mailmindlin <mailmindlin@users.noreply.github.com>
2024-01-22 23:05:04 -0500

    Tabs

--- MOEnet-client/src/main/java/frc/robot/sensor/vision/Vision.java
+++ MOEnet-client/src/main/java/frc/robot/sensor/vision/Vision.java
@@ -1,6 +1,7 @@
 package frc.robot.sensor.vision;
 
 import java.util.Objects;
+import java.util.List;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
@@ -12,100 +13,90 @@ import edu.wpi.first.math.kinematics.Odometry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.sensor.vision.moenet.MoeNet;
+import frc.robot.sensor.vision.io.ObjectDetection;
 
 public class Vision implements PoseSensor {
-    public static Builder builder() {
-        return new Builder();
-    }
+	public static Builder builder() {
+		return new Builder();
+	}
 
-    public static class Builder {
-        private NetworkTableInstance nt = NetworkTableInstance.getDefault();
-        private AprilTagFieldLayout apriltags;
-        private Odometry<?> odometry;
+	public static class Builder {
+		private NetworkTableInstance nt = NetworkTableInstance.getDefault();
+		private AprilTagFieldLayout apriltags;
+		private Odometry<?> odometry;
 
-        public Builder setNetworkTables(NetworkTableInstance nt) {
-            return this;
-        }
-        public Builder setAprilTags(AprilTagFields field) {
-            Objects.requireNonNull(field, "AprilTag field");
-            return setAprilTags(field.loadAprilTagLayoutField());
-        }
-        public Builder setAprilTags(AprilTagFieldLayout apriltags) {
-            return this;
-        }
+		public Builder setNetworkTables(NetworkTableInstance nt) {
+			return this;
+		}
+		public Builder setAprilTags(AprilTagFields field) {
+			Objects.requireNonNull(field, "AprilTag field");
+			return setAprilTags(field.loadAprilTagLayoutField());
+		}
+		public Builder setAprilTags(AprilTagFieldLayout apriltags) {
+			return this;
+		}
 
-        public Builder addMoenet() {
-            return addMoenet(MoeNet.DEFAULT_NAME);
-        }
-        public Builder addMoenet(String name) {
-            return this;
-        }
+		public Builder addMoenet() {
+			return addMoenet(MoeNet.DEFAULT_NAME);
+		}
+		public Builder addMoenet(String name) {
+			return this;
+		}
 
-        public Builder addCamera(VisionSystem vision) {
-            Objects.requireNonNull(vision);
-            return this;
-        }
+		public Builder addCamera(VisionSystem vision) {
+			Objects.requireNonNull(vision);
+			return this;
+		}
 
-        public Builder addOdometry(Odometry<?> odometry) {
-            this.odometry = Objects.requireNonNull(odometry);
-            return this;
-        }
+		public Vision build() {
+			return new Vision();
+		}
+	}
 
-        public Vision build() {
-            return new Vision();
-        }
-    }
+	private Pose3d fieldToRobot = new Transform3d();
+	private Transform3d odomToRobot = new Transform3d();
+	private List<ObjectDetection> objects = new ArrayList<>();
+	private MoeNet moenet;
 
-    private Transform3d odometryCorrection = new Transform3d();
-    private MoeNet moenet;
+	private Vision() {
 
-    private Vision() {
+	}
 
-    }
+	/**
+	 * Tell the vision system that we know where we are
+	 * @param pose
+	 */
+	public void setPose(Pose3d pose) {
+		//TODO
+	}
 
-    /**
-     * Tell the vision system that we know where we are
-     * @param pose
-     */
-    public void setPose(Pose3d pose) {
-        //TODO
-    }
+	public Transform3d getOdometryCorrection() {
+		return null;
+	}
 
-    private void updateOdometry(Pose2d odometryPose) {
-        updateOdometry(new Pose3d(odometryPose));
-    }
+	/**
+	 * Periodic update.
+	 * This should be called AFTER the odometry is refreshed.
+	 */
+	public void periodic() {
 
-    private void updateOdometry(Pose3d odometryPose) {
+	}
 
-    }
+	@Override
+	public Pose3d getPose3d() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getPose3d'");
+	}
 
-    public Transform3d getOdometryCorrection() {
-        return null;
-    }
+	@Override
+	public Pose2d getPose2d() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getPose2d'");
+	}
 
-    /**
-     * Periodic update.
-     * This should be called AFTER the odometry is refreshed.
-     */
-    public void periodic() {
-
-    }
-
-    @Override
-    public Pose3d getPose3d() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPose3d'");
-    }
-
-    @Override
-    public Pose2d getPose2d() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPose2d'");
-    }
-
-    @Override
-    public boolean isRelative() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isRelative'");
-    }
+	@Override
+	public boolean isRelative() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'isRelative'");
+	}
 }

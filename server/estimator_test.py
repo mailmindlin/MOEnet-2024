80ca3cf694a535c784bab7c8de08ee7b90bddc0b
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-01-31 00:56:37 -0500

    Comment estimator_test

--- server/estimator_test.py
+++ server/estimator_test.py
@@ -1,14 +1,15 @@
 from unittest import TestCase
-from estimator import PoseEstimator, Transform3d, MsgPose, Pose3d, Rotation3d, Translation3d, Clock, Twist3d
+from estimator import PoseEstimator, Transform3d, MsgPose, Pose3d, Rotation3d, Translation3d, Clock
 from typedef.geom import Pose3d, Twist3d, Translation3d
 # Tests
 class PoseEstimatorTest(TestCase):
     def setUp(self) -> None:
+        # Create a clock that doesn't update
         class FakeClock(Clock):
             def __init__(self) -> None:
                 super().__init__()
                 self.time = 0
-            def now(self):
+            def now_ns(self):
                 return self.time
         self.clock = FakeClock()
     
@@ -17,13 +18,18 @@ class PoseEstimatorTest(TestCase):
             clock=self.clock,
             history_duration=10.0
         )
+        # Start at f2r (0, 0, 0) t=0
         f2r_0 = Pose3d(Translation3d(0, 0, 0), Rotation3d())
-        f2r_2 = Pose3d(Translation3d(2, 0, 0), Rotation3d())
         estimator.record_f2r(Transform3d(), MsgPose(timestamp=0, view_mat=None, pose=f2r_0, poseCovariance=None, twist=Twist3d(), twistCovariance=None))
+        # End at f2r (0, 0, 0) t=2s
+        f2r_2 = Pose3d(Translation3d(2, 0, 0), Rotation3d())
         estimator.record_f2r(Transform3d(), MsgPose(timestamp=int(2e9), view_mat=None, pose=f2r_2, poseCovariance=None, twist=Twist3d(), twistCovariance=None))
+
+        # Check simple lookups
         assert estimator.field_to_robot(0) == Pose3d()
         assert estimator.field_to_robot(2e9) == Pose3d(Translation3d(x=2,y=0,z=0), Rotation3d())
-        # Check lerp
+
+        # Check lerp (t=1s)
         assert estimator.field_to_robot(1e9) == Pose3d(Translation3d(x=1,y=0,z=0), Rotation3d())
 
         # We haven't provided any odometry info, so it should be empty

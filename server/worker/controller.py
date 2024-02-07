12dcbf398a1e680ab8983bafd695e6eb01e9d598
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-02-07 00:49:37 -0500

    WorherHandle based on Subprocess
    Share some code

--- server/worker/controller.py
+++ server/worker/controller.py
@@ -280,8 +280,107 @@ class WorkerManager:
 		"Iterate through camera handles"
 		return self._workers.__iter__()
 
+class WorkerHandle(Subprocess[worker.WorkerMsg, worker.AnyCmd, worker.AnyMsg]):
+	def __init__(self, name: str, config: worker.WorkerInitConfig, *, log: logging.Logger | None = None, ctx: BaseContext | None = None, datalog: Optional['DataLog'] = None, vidq: Optional['Queue'] = None):
+		if ctx is None:
+			ctx = get_context('spawn')
+		
+		super().__init__(
+			name,
+			log=child_logger(name, log),
+			cmd_queue=0,
+			msg_queue=0,
+			daemon=True, ctx=ctx)
 
-class WorkerHandle:
+		self.datalog = datalog
+		if datalog is not None:
+			logConfig = StringLogEntry(self.datalog, f'worker/{name}/config')
+			logConfig.append(config.model_dump_json())
+			logConfig.finish()
+			del logConfig
+
+			self.logStatus = IntegerLogEntry(self.datalog, f'worker/{name}/status')
+			self.logLog = StringLogEntry(self.datalog, f'worker/{name}/log')
+
+		self.config = config
+		self.video_queue = vidq
+		self._require_flush_id = 0
+		self._last_flush_id = 0
+		self._restarts = 0
+		self.robot_to_camera = config.robot_to_camera
+		self.add_handler(worker.MsgLog, self._handle_log)
+		self.add_handler(worker.MsgFlush, self._handle_flush)
+		self.add_handler(worker.MsgChangeState, self._handle_changestate)
+
+	def _get_args(self):
+		return (
+			self.config,
+			self.msg_queue,
+			self.cmd_queue,
+			self.video_queue,
+		)
+	@property
+	def target(self):
+		from worker.worker import main as worker_main
+		return worker_main
+	
+	def make_stop_command(self) -> worker.AnyCmd:
+		return worker.CmdChangeState(target=worker.WorkerState.STOPPED)
+	def enable_stream(self, stream: str, enable: bool):
+		self.send(worker.CmdEnableStream(stream=stream, enable=enable))
+	
+	def flush(self):
+		self._require_flush_id += 1
+		self.send(worker.CmdFlush(id=self._require_flush_id))
+	
+	# Message handlers
+	def _handle_log(self, packet: worker.MsgLog):
+		log = self.log if packet.name == 'root' else self.log.getChild(packet.name)
+		log.log(packet.level, packet.msg)
+		if self.datalog is not None:
+			self.logLog.append(f'[{logging.getLevelName(packet.level)}]{packet.name}:{packet.msg}')
+	
+	def _handle_flush(self, packet: worker.MsgFlush):
+		self.log.debug('Finished flush %d', packet.id)
+		self._last_flush_id = max(self._last_flush_id, packet.id)
+	
+	def _handle_changestate(self, packet: worker.MsgChangeState):
+		self.child_state = packet.current
+		if self.datalog is not None:
+			self.logStatus.append(int(self.child_state))
+	
+	def handle_default(self, msg: worker.WorkerMsg) -> worker.AnyMsg | None:
+		if self._last_flush_id < self._require_flush_id:
+			# Packets are invalidated by a flush
+			self.log.info("Skipping packet (flushed)")
+			return None
+		else:
+			return msg
+	
+	def handle_dead(self):
+		optional = self.config.retry.optional
+		self._restarts += 1
+		can_retry = self._restarts < self.config.retry.restart_tries
+
+		if optional:
+			self.log.warning('Unexpectedly exited')
+			self.child_state = worker.WorkerState.STOPPING
+		else:
+			self.log.error('Unexpectedly exited')
+		# Cleanup process
+		self.stop(ask=False)
+		if can_retry:
+			self.child_state = worker.WorkerState.STOPPED
+			self.log.info("Restarting (%d of %d)", self._restarts, self.config.retry.restart_tries - 1)
+			# TODO: honor connection_delay?
+			self.start()
+		elif optional:
+			self.child_state = worker.WorkerState.STOPPED
+		else:
+			self.child_state = worker.WorkerState.FAILED
+			raise RuntimeError(f'Camera {self.name} unexpectedly exited')
+		
+class WorkerHandle0:
 	"Manage a single camera worker"
 	
 	proc: Process

51636389b2f52a8ce90440fb6f5a4d5d2f3944ca
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-02-12 21:14:31 -0500

    Move stuff into utils

---
+++ server/estimator/util/__init__.py
@@ -0,0 +1,1 @@
+from .interpolate import interpolate_pose3d
---
+++ server/estimator/util/queue.py
@@ -0,0 +1,28 @@
+from typing import TypeVar, Generic
+import heapq
+
+T = TypeVar('T')
+class Heap(Generic[T]):
+    def __init__(self) -> None:
+        self.data: list[T] = list()
+    
+    def push(self, value: T):
+        heapq.heappush(self.data, value)
+    
+    def pop(self) -> T:
+        return heapq.heappop(self.data)
+    
+    def peek(self) -> T | None:
+        try:
+            return self.data[0]
+        except KeyError:
+            return None
+    
+    def __bool__(self):
+        return len(self.data) > 0
+    
+    def __len__(self):
+        return len(self.data)
+
+    def clear(self):
+        pass

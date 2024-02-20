9c8323c722c2160fa015d39c0241ca0e1cfc3321
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-02-19 22:07:16 -0500

    Cleanup duplicate code

--- server/estimator/util/__init__.py
+++ server/estimator/util/__init__.py
@@ -1,10 +1,1 @@
-from typedef.geom import Pose3d
-
-def interpolate_pose3d(a: Pose3d, b: Pose3d, t: float) -> Pose3d:
-	"Interpolate between `Pose3d`s"
-	if t <= 0:
-		return a
-	if t >= 1:
-		return b
-	twist = a.log(b)
-	return a.exp(twist * t)
+from .lerp import lerp_pose3d, lerp_transform3d

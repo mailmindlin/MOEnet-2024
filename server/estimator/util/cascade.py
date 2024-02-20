84de87c802764abc608fc7482c15789e06ed9771
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-02-19 21:59:21 -0500

    Start cascading inferences

---
+++ server/estimator/util/cascade.py
@@ -0,0 +1,107 @@
+from typing import TypeVar, Generic, Callable
+import abc
+
+T = TypeVar('T')
+R = TypeVar('R')
+class Tracked(Generic[T], abc.ABC):
+    value: T
+
+    is_static = False
+
+    @abc.abstractproperty
+    def is_fresh(self) -> bool:
+        return False
+
+    def refresh(self) -> 'Tracked[T]':
+        assert self.is_fresh
+        return self
+
+    def map(self, func: Callable[[T], R]) -> 'Tracked[R]':
+        if self.is_static and self.is_fresh:
+            return StaticValue(func(self.value))
+        return Derived(func, self)
+
+class StaticValue(Tracked[T]):
+    def __init__(self, value: T) -> None:
+        super().__init__()
+        self.value = value
+    
+    is_static = True
+
+    @property
+    def is_fresh(self):
+        return True
+    
+    def refresh(self):
+        return self
+    
+    def map(self, func: Callable[[T], R]) -> Tracked[R]:
+        return StaticValue(func(self.value))
+    
+    def __hash__(self) -> int:
+        return hash(self.value)
+    
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, StaticValue):
+            return __value.value == self.value
+        elif isinstance(__value, Tracked):
+            return (
+                __value.is_static
+                and __value.is_fresh
+                and __value.value == self.value
+            )
+        else:
+            return False
+
+undefined = object()
+class Derived(Tracked[T]):
+    def __init__(self, func: Callable, *args: Tracked):
+        super().__init__()
+        self._func = func
+        self._args = args
+        self._last_args = None
+        self._last_value = undefined
+    
+    @property
+    def is_static(self):
+        return all(arg.is_static for arg in self._args)
+    
+    @property
+    def is_fresh(self):
+        if not all(arg.is_fresh for arg in self._args):
+            return False
+        if self._last_args is not None:
+            for arg, last_arg in zip(self._args, self._last_args):
+                if arg.value != last_arg:
+                    return False
+        return True
+    
+    @property
+    def value(self) -> T:
+        if self._last_value is undefined:
+            self._last_args = [arg.value for arg in self._args]
+            self._last_value = self._func(*self._last_args)
+        return self._last_value
+    
+    def refresh(self) -> Tracked[T]:
+        replace = False
+        args_static = True
+        res_args: list[Tracked] = list()
+        for arg in self._args:
+            arg1 = arg.refresh()
+            args_static &= arg1.is_static and arg1.is_fresh
+            replace |= (arg1 is not arg)
+            res_args.append(arg1)
+        
+        if replace:
+            # Rebuild if args changed
+            res = Derived(self._func, *res_args)
+        else:
+            res = self
+            res._last_args = None
+            res._last_value = undefined
+        
+        if args_static:
+            # Simplify
+            return StaticValue(res.value)
+        return res
---
+++ server/estimator/util/cascade_test.py
@@ -0,0 +1,63 @@
+from unittest import TestCase
+from .cascade import Tracked, StaticValue, Derived
+
+class TestStatic(TestCase):
+	def test_simple(self):
+		t = StaticValue(5.0)
+
+		self.assertTrue(t.is_fresh)
+		self.assertTrue(t.is_static)
+		self.assertEqual(t.value, 5.0)
+		self.assertEqual(t.refresh(), t)
+	
+	def test_map(self):
+		t = StaticValue(5.0)
+		r = t.map(lambda x: x * x)
+
+		self.assertTrue(r.is_fresh)
+		self.assertTrue(r.is_static)
+		self.assertEqual(r.value, 25.0)
+		self.assertEqual(r.refresh(), r)
+
+class TestDerive(TestCase):
+	def test_map(self):
+		class SampleTracked(Tracked[int]):
+			def __init__(self, value: int):
+				super().__init__()
+				self.value = value
+				self.value_next = value
+			
+			@property
+			def is_fresh(self):
+				return self.value == self.value_next
+
+			def refresh(self) -> Tracked[int]:
+				self.value = self.value_next
+				return self
+
+		a = SampleTracked(5)
+		assert a.is_fresh
+		assert not a.is_static
+		assert a.value == 5
+
+		b = a.map(lambda x: x * x)
+		assert b.is_fresh
+		assert not b.is_static
+		assert b.value == 25
+
+		a.value_next = 6
+		assert not b.is_fresh
+		assert not a.is_fresh
+		# Check no auto-refresh
+		assert a.value == 5
+		assert b.value == 25
+
+		a = a.refresh()
+		assert a.value == 6
+		assert a.is_fresh
+		assert b.value == 25
+		assert not b.is_fresh
+
+		b = b.refresh()
+		assert b.value == 36
+		assert b.is_fresh

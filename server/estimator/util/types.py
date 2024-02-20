b7ac9f61c956b0cd4cbb89efe0d3192d59c8b941
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-02-19 21:59:32 -0500

    Fill out generic filter

--- server/estimator/util/types.py
+++ server/estimator/util/types.py
@@ -1,4 +1,6 @@
 from typing import Protocol, TypeVar, Generic
+from datetime import timedelta
+from abc import ABC, abstractmethod
 from util.timestamp import Timestamp
 
 
@@ -6,10 +8,23 @@ class HasTimestamp(Protocol):
 	ts: Timestamp
 
 
-S = TypeVar('S', bound=HasTimestamp)
+M = TypeVar('M', bound=HasTimestamp)
+# S = TypeVar('S', bound=HasTimestamp)
 
-class Filter(Generic[S]):
-	pass
+class Filter(ABC, Generic[M]):
+	def validate_delta(self, delta: timedelta):
+		pass
+
+	def clear(self):
+		pass
+
+	@abstractmethod
+	def observe(self, measurement: M):
+		pass
+
+	@abstractmethod
+	def predict(self, now: Timestamp, delta: timedelta):
+		pass
 	
 T = TypeVar('T')
 class InterpolableData(Generic[T]):

bc55252a9f0b912131c3b18e8e45f56fc1baeb6a
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-02-26 14:42:14 -0500

    Add/fix coordinate system overloads

--- server/worker/node/coords.py
+++ server/worker/node/coords.py
@@ -2,7 +2,7 @@ from typing import overload
 import numpy as np
 from wpimath.geometry import CoordinateSystem
 
-from typedef.geom import Rotation3d, Transform3d, Pose3d, Twist3d
+from typedef.geom import Rotation3d, Transform3d, Pose3d, Twist3d, Translation3d
 
 
 APRILTAG_BASE_ROTATION = Rotation3d([0,1,0], np.deg2rad(180))
@@ -39,7 +39,15 @@ def cv2_to_apriltag(tf_cv2: Transform3d) -> Transform3d:
 		rot_at,
     )
 
-def wpi_to_cv2(tf_wpi: Transform3d) -> Transform3d:
+@overload
+def wpi_to_cv2(tf_cv2: Transform3d) -> Transform3d: ...
+@overload
+def wpi_to_cv2(tf_cv2: Pose3d) -> Pose3d: ...
+@overload
+def wpi_to_cv2(tf_cv2: Rotation3d) -> Rotation3d: ...
+@overload
+def wpi_to_cv2(tf_cv2: Translation3d) -> Translation3d: ...
+def wpi_to_cv2(tf_wpi: Transform3d | Pose3d | Rotation3d | Translation3d):
 	return CoordinateSystem.convert(tf_wpi, CoordinateSystem.NWU(), CoordinateSystem.EDN())
 
 @overload
@@ -47,8 +55,6 @@ def cv2_to_wpi(tf_cv2: Transform3d) -> Transform3d: ...
 @overload
 def cv2_to_wpi(tf_cv2: Pose3d) -> Pose3d: ...
 @overload
-def cv2_to_wpi(tf_cv2: Twist3d) -> Twist3d: ...
-@overload
 def cv2_to_wpi(tf_cv2: Rotation3d) -> Rotation3d: ...
-def cv2_to_wpi(arg0: Transform3d | Pose3d | Twist3d | Rotation3d):
+def cv2_to_wpi(arg0: Transform3d | Pose3d | Rotation3d):
 	return CoordinateSystem.convert(arg0, CoordinateSystem.EDN(), CoordinateSystem.NWU())

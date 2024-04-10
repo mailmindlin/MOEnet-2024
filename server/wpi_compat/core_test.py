c7c926721c4898c324d5983e34f5849985bf5be1
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-09 20:54:43 -0400

    Fix test case props

--- server/wpi_compat/core_test.py
+++ server/wpi_compat/core_test.py
@@ -2,17 +2,17 @@ from ntcore import NetworkTableInstance
 from unittest import TestCase
 
 class NtTestCase(TestCase):
+    server: NetworkTableInstance
+    client: NetworkTableInstance
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.server = None
-        self.client = None
 
     def setUp(self) -> None:
-        if self.server is None:
+        if getattr(self, 'server', None) is None:
             self.server = NetworkTableInstance.create()
             self.server.startLocal()
             self.server.startServer(listen_address='localhost', port4=5810)
-        if self.client is None:
+        if getattr(self, 'client', None) is None:
             self.client = self.server
             # self.client = NetworkTableInstance.create()
             # self.client.setServer('localhost', 5810)
@@ -22,12 +22,12 @@ class NtTestCase(TestCase):
         
         return super().setUp()
     def tearDown(self) -> None:
-        if self.server is not None:
+        if getattr(self, 'server', None) is not None:
             self.server.stopServer()
-            self.server = None
-        if self.client is not None:
+            del self.server
+        if getattr(self, 'client', None) is not None:
             self.client.stopClient()
-            self.client = None
+            del self.client
 
         return super().tearDown()
 
@@ -40,7 +40,7 @@ class NtTestRelay(NtTestCase):
             pub.set(5)
             assert sub.get() == 5
     
-    def test_relay_s2c(self):
+    def test_relay_c2s(self):
         with (
             self.client.getIntegerTopic("test").publish() as pub,
             self.server.getIntegerTopic("test").subscribe(1) as sub

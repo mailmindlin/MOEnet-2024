7057c616cfb66ba9e200bbfed0d2701d2da0acfa
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-09 21:15:50 -0400

    SLAM generates odom message

--- server/worker/session.py
+++ server/worker/session.py
@@ -7,7 +7,7 @@ from typedef.geom import Pose3d, Translation3d, Rotation3d, Quaternion, Twist3d
 from util.clock import WallClock
 from util.timestamp import Timestamp
 
-from .msg import MsgFrame, ObjectDetection, MsgDetections, MsgPose
+from .msg import MsgFrame, ObjectDetection, MsgDetections, MsgPose, MsgOdom, Pose3dCov, Twist3dCov
 
 if TYPE_CHECKING:
     import logging
@@ -215,49 +215,54 @@ class MoeNetSession:
         ROTATION_COV = 1e-4
         ANG_VEL_COV = 1e-3
 
-        return MsgPose(
+        return MsgOdom(
             timestamp=timestamp,
             # Not sure if we need this property
-            view_mat=vio_out.pose.asMatrix(),
+            # view_mat=vio_out.pose.asMatrix(),
+            
             # I wish there was a better way to do this, but spectacularAI types are native wrappers,
             # and they can't be nicely shared between processes
-            pose=Pose3d(
-                translation=Translation3d(
-                    x = vio_out.pose.position.x,
-                    y = vio_out.pose.position.y,
-                    z = vio_out.pose.position.z,
+            pose=Pose3dCov(
+                Pose3d(
+                    translation=Translation3d(
+                        x = vio_out.pose.position.x,
+                        y = vio_out.pose.position.y,
+                        z = vio_out.pose.position.z,
+                    ),
+                    rotation=Rotation3d(Quaternion(
+                        w = vio_out.pose.orientation.w,
+                        x = vio_out.pose.orientation.x,
+                        y = vio_out.pose.orientation.y,
+                        z = vio_out.pose.orientation.z,
+                    ))
                 ),
-                rotation=Rotation3d(Quaternion(
-                    w = vio_out.pose.orientation.w,
-                    x = vio_out.pose.orientation.x,
-                    y = vio_out.pose.orientation.y,
-                    z = vio_out.pose.orientation.z,
-                ))
+                np.asarray([
+                    [pc[0, 0], pc[0, 1], pc[0, 2], 0, 0, 0],
+                    [pc[1, 0], pc[1, 1], pc[1, 2], 0, 0, 0],
+                    [pc[2, 0], pc[2, 1], pc[2, 2], 0, 0, 0],
+                    [0, 0, 0, ROTATION_COV, 0, 0],
+                    [0, 0, 0, 0, ROTATION_COV, 0],
+                    [0, 0, 0, 0, 0, ROTATION_COV],
+                ], dtype=np.float32),
             ),
-            poseCovariance=np.asarray([
-                [pc[0, 0], pc[0, 1], pc[0, 2], 0, 0, 0],
-                [pc[1, 0], pc[1, 1], pc[1, 2], 0, 0, 0],
-                [pc[2, 0], pc[2, 1], pc[2, 2], 0, 0, 0],
-                [0, 0, 0, ROTATION_COV, 0, 0],
-                [0, 0, 0, 0, ROTATION_COV, 0],
-                [0, 0, 0, 0, 0, ROTATION_COV],
-            ], dtype=np.float32),
-            twist=Twist3d(
-                dx = vio_out.velocity.x,
-                dy = vio_out.velocity.y,
-                dz = vio_out.velocity.z,
-                rx = vio_out.angularVelocity.x,
-                ry = vio_out.angularVelocity.y,
-                rz = vio_out.angularVelocity.z,
-            ),
-            twistCovariance=np.asarray([
-                [vc[0, 0], vc[0, 1], vc[0, 2], 0, 0, 0],
-                [vc[1, 0], vc[1, 1], vc[1, 2], 0, 0, 0],
-                [vc[2, 0], vc[2, 1], vc[2, 2], 0, 0, 0],
-                [0, 0, 0, ANG_VEL_COV, 0, 0],
-                [0, 0, 0, 0, ANG_VEL_COV, 0],
-                [0, 0, 0, 0, 0, ANG_VEL_COV],
-            ], dtype=np.float32),
+            twist=Twist3dCov(
+                Twist3d(
+                    dx = vio_out.velocity.x,
+                    dy = vio_out.velocity.y,
+                    dz = vio_out.velocity.z,
+                    rx = vio_out.angularVelocity.x,
+                    ry = vio_out.angularVelocity.y,
+                    rz = vio_out.angularVelocity.z,
+                ),
+                np.asarray([
+                    [vc[0, 0], vc[0, 1], vc[0, 2], 0, 0, 0],
+                    [vc[1, 0], vc[1, 1], vc[1, 2], 0, 0, 0],
+                    [vc[2, 0], vc[2, 1], vc[2, 2], 0, 0, 0],
+                    [0, 0, 0, ANG_VEL_COV, 0, 0],
+                    [0, 0, 0, 0, ANG_VEL_COV, 0],
+                    [0, 0, 0, 0, 0, ANG_VEL_COV],
+                ], dtype=np.float32),
+            )
         )
 
     def flush(self):

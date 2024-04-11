25d99b00eec989576f66d2f4f86983a977be4683
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-11 18:23:12 -0400

    InterpolatedView

--- server/estimator/util/interpolated.py
+++ server/estimator/util/interpolated.py
@@ -1,10 +1,7 @@
-from typing import TYPE_CHECKING, TypeVar, Callable, Generic, Protocol, Self, overload, Any
-from collections.abc import Hashable, ItemsView
-import enum
+from typing import TYPE_CHECKING, Callable, Protocol, Self, overload, Reversible
+from collections.abc import Hashable, Sequence
 from dataclasses import dataclass
 
-from util.timestamp import Timestamp
-from .types import InterpolableData
 from .cascade import Tracked
 
 undefined = object()
@@ -20,24 +17,19 @@ else:
 class Comparable(Protocol):
     def __lt__(self, other: Self, /) -> bool: ...
 
-D = TypeVar('D', bound=Comparable)
+class Delta(Comparable, Protocol):
+	def __truediv__(self, other: Self, /) -> float: ...
 
-
-class Key(Hashable, Comparable, Protocol[D]):
+class Key[D: Delta](Hashable, Comparable, Protocol):
 	"Key type"
-	@overload
-	def __sub__(self, other: D, /) -> Self: ...
-	@overload
+	# @overload
+	# def __sub__(self, delta: D, /) -> Self: ...
+	# @overload
 	def __sub__(self, other: Self, /) -> D: ...
-	def __sub__(self, other: Self | D, /) -> Self | D: ...
 
-K = TypeVar('K', bound=Key)
-V = TypeVar('V')
-T = TypeVar('T')
-
-class InterpolateResult(Generic[V]):
+class InterpolateResult[V]:
 	@staticmethod
-	def wrap(buffer: 'InterpolatingBuffer[K, V, D]', key: K, bottomBound: tuple[K,V] | None, topBound: tuple[K,V] | None):
+	def wrap[K: Key](view: 'InterpolatingView[K, V]', key: K, bottomBound: tuple[K,V] | None, topBound: tuple[K,V] | None):
 		# Return null if neither sample exists, and the opposite bound if the other is null
 		if (topBound is not None) and (bottomBound is not None):
 			if topBound[0] == key:
@@ -49,9 +41,10 @@ class InterpolateResult(Generic[V]):
 			# Otherwise, interpolate. Because T is between [0, 1], we want the ratio of (the difference
 			# between the current time and bottom bound) and (the difference between top and bottom
 			# bounds).
+			
 			p: float = (key - bottomBound[0]) / (topBound[0] - bottomBound[0])
 			return InterpolateBetween(
-				buffer,
+				view,
 				bottomBound,
 				topBound,
 				p,
@@ -64,62 +57,66 @@ class InterpolateResult(Generic[V]):
 			# No bounds
 			return InterpolateEmpty()
 	
-	def get(self, default: T) -> V | T: ...
+	def get[T](self, default: T) -> V | T: ...
 
-class InterpolateEmpty(InterpolateResult[V]):
+class InterpolateEmpty[V](InterpolateResult[V]):
 	"Empty InterpolateResult"
-	def get(self, default: T) -> T:
+	def get[T](self, default: T) -> T:
 		return default
 
 @dataclass
-class InterpolateBefore(InterpolateResult[V]):
+class InterpolateBefore[V](InterpolateResult[V]):
 	right: V
-	def get(self, default: T) -> V:
+	def get[T](self, default: T) -> V | T:
 		return self.right
 
 @dataclass
-class InterpolateAfter(InterpolateResult[V]):
+class InterpolateAfter[V](InterpolateResult[V]):
 	left: V
-	def get(self, default: T) -> V:
+	def get[T](self, default: T) -> V | T:
 		return self.left
 
 @dataclass
-class InterpolateExact(InterpolateResult[V]):
+class InterpolateExact[V](InterpolateResult[V]):
 	value: V
-	def get(self, default: T) -> V:
+	def get[T](self, default: T) -> V | T:
 		return self.value
 
 @dataclass
-class InterpolateBetween(InterpolateResult[V], Generic[K, V, D]):
-	buffer: 'InterpolatingBuffer[K, V, D]'
+class InterpolateBetween[K: Key, V](InterpolateResult[V]):
+	buffer: 'InterpolatingView[K, V]'
 	left: tuple[K,V]
 	right: tuple[K,V]
 	p: float
 
-	def get(self, default: T) -> V:
+	def get[T](self, default: T) -> V | T:
 		return self.buffer._lerp(
 			self.left[1],
 			self.right[1],
 			self.p,
 		)
 
-class TrackedInterpolation(Tracked[V | T], Generic[K, V, T]):
+class TrackedInterpolation[K: Key, V, T](Tracked[V | T]):
 	"Track a"
-	def __init__(self, buffer: 'InterpolatingBuffer[K, V, Any]', key: K, default: T) -> None:
+	def __init__(self, buffer: 'InterpolatingView[K, V]', key: K, default: T) -> None:
 		super().__init__()
 		self._buffer = buffer
 		self.key = key
 		self.default = default
 		self._bottom, self._top = self._buffer._get_bounds(self.key)
-		self.value = InterpolateResult.wrap(buffer, key, self._bottom, self._top).get(default)
-		self._buffer_modcount = self._buffer._modcount
+		self._value = InterpolateResult.wrap(buffer, key, self._bottom, self._top).get(default)
+		self._buffer_modcount = self._buffer.modcount
+	
+	@property
+	def current(self):
+		return self._value
 	
 	def __repr__(self) -> str:
 		return f'TrackedInterpolation(buffer={self._buffer!r}, key={self.key!r}, default={self.default!r})'
 
 	@property
 	def is_fresh(self):
-		buf_mc = self._buffer._modcount
+		buf_mc = self._buffer.modcount
 		if buf_mc == self._buffer_modcount:
 			return True
 		
@@ -131,7 +128,7 @@ class TrackedInterpolation(Tracked[V | T], Generic[K, V, T]):
 		return True
 
 	def refresh(self):
-		buf_mc = self._buffer._modcount
+		buf_mc = self._buffer.modcount
 		if buf_mc == self._buffer_modcount:
 			# Fresh
 			return self
@@ -144,46 +141,51 @@ class TrackedInterpolation(Tracked[V | T], Generic[K, V, T]):
 			else:
 				self._top = top
 				self._bottom = bottom
-				self.value = InterpolateResult.wrap(self._buffer, self.key, bottom, top).get(self.default)
+				self._value = InterpolateResult.wrap(self._buffer, self.key, bottom, top).get(self.default)
 		
 		return self
 
+class SequenceQueue[K: Key, V](Protocol):
+	def __len__(self) -> int: ...
+	def bisect_right(self, key: K, /) -> int: ...
+	def peekitem(self, index: int, /) -> tuple[K, V]: ...
+	def keys(self) -> Reversible[K]: ...
+	def items(self) -> Reversible[tuple[K, V]]: ...
 
-class InterpolatingBuffer(Generic[K, V, D]):
-	def __init__(self, historyLength: D, lerp: Callable[[V, V, float], V]):
-		self.historyLength = historyLength
-		self._snapshots: SortedDict[K, V] = SortedDict()
-		self._lerp = lerp
-		self._modcount = 0
-	
-	def _clean_up(self, time: K):
-		"Removes samples older than our current history size."
-		first = time - self.historyLength
-		while len(self._snapshots) > 0:
-			entry: tuple[K, V] = self._snapshots.peekitem(0)
-			key = entry[0]
-			if key < first:
-				del self._snapshots[key]
-			else:
-				break
-
+class SortedSequenceAdapter[K: Key, V](SequenceQueue[K, V]):
+	def __init__(self, backing: Sequence[V], keyfunc: Callable[[V], K]):
+		self.backing = backing
+		self._keyfunc = keyfunc
 	def __len__(self):
-		return len(self._snapshots)
+		return len(self.backing)
+	def bisect_right(self, key: K) -> int:
+		from bisect import bisect_right
+		return bisect_right(self.backing, key, key=self._keyfunc)
+	def peekitem(self, index: int) -> tuple[K, V]:
+		v = self.backing[index]
+		k = self._keyfunc(v)
+		return (k, v)
+	def keys(self) -> Reversible[K]:
+		raise NotImplementedError()
+	def items(self) -> Reversible[tuple[K, V]]:
+		raise NotImplementedError()
+
+
+class InterpolatingView[K: Key, V]:
+	def __init__(self, queue: SequenceQueue[K, V], lerp: Callable[[V, V, float], V]) -> None:
+		self._queue = queue
+		self._lerp = lerp
+		self.modcount = 0
+		"Count modifications, for `Tracked` caching"
 	
-	def clear(self):
-		self._snapshots.clear()
-		self._modcount += 1
-	
-	def add(self, key: K, value: V):
-		self._clean_up(key)
-		self._snapshots[key] = value
-		self._modcount += 1
+	def __len__(self):
+		return len(self._queue)
 	
 	def _get_bounds(self, key: K) -> tuple[tuple[K,V] | None, tuple[K,V] | None]:
 		# Special case for when the requested time is the same as a sample
-		key_idx = self._snapshots.bisect_right(key)
+		key_idx = self._queue.bisect_right(key)
 		if key_idx > 0:
-			bottomBound = self._snapshots.peekitem(key_idx - 1)
+			bottomBound = self._queue.peekitem(key_idx - 1)
 			bottomKey, _ = bottomBound
 			# If an exact match exists, it will be in bottomBound
 			if bottomKey == key:
@@ -191,15 +193,15 @@ class InterpolatingBuffer(Generic[K, V, D]):
 		else:
 			bottomBound = None
 		
-		if key_idx >= len(self._snapshots):
+		if key_idx >= len(self._queue):
 			# Index is to the right of any value
 			topBound = None
 		else:
-			topBound = self._snapshots.peekitem(key_idx)
+			topBound = self._queue.peekitem(key_idx)
 		return bottomBound, topBound
 	
 	def _get_ex(self, key: K):
-		if len(self._snapshots) == 0:
+		if len(self._queue) == 0:
 			return InterpolateEmpty()
 		bottomBound, topBound = self._get_bounds(key)
 
@@ -208,7 +210,7 @@ class InterpolatingBuffer(Generic[K, V, D]):
 			return InterpolateExact(topBound[1])
 		
 		return InterpolateResult.wrap(self, key, bottomBound, topBound)
-			
+	
 	@overload
 	def get(self, /, key: K) -> V | None: ...
 	@overload
@@ -227,16 +229,45 @@ class InterpolatingBuffer(Generic[K, V, D]):
 	@overload
 	def track(self, key: K, default: V) -> Tracked[V]: ...
 	@overload
-	def track(self, key: K, default: T) -> Tracked[V | T]: ...
-	def track(self, key: K, default: T | None = None) -> Tracked[V | T | None]:
+	def track[T](self, key: K, default: T) -> Tracked[V | T]: ...
+	def track[T](self, key: K, default: T = None) -> Tracked[V | T | None]:
 		return TrackedInterpolation(self, key, default)
 	
 	@overload
 	def latest(self) -> Tracked[V | None]: ...
 	@overload
-	def latest(self, default: T) -> Tracked[V | T]: ...
-	def latest(self, default: T = None) -> Tracked[V | T]:
+	def latest[T](self, default: T) -> Tracked[V | T]: ...
+	def latest[T](self, default: T = None) -> Tracked[V | T]:
 		raise NotImplementedError()
 		
-	def getInternalBuffer(self) -> list[tuple[K, V]]:
-		return list(self._snapshots.items())
+	def keys(self):
+		return self._queue.keys()
+	
+	def items(self):
+		return self._queue.items()
+
+class InterpolatingBuffer[K: Key, V, D: Comparable](InterpolatingView[K, V]):
+	_queue: SortedDict[K, V]
+	def __init__(self, historyLength: D, lerp: Callable[[V, V, float], V]):
+		super().__init__(SortedDict(), lerp)
+		self.historyLength = historyLength
+	
+	def _clean_up(self, last_key: K):
+		"Removes samples older than our current history size."
+		first = last_key - self.historyLength
+		while len(self._queue) > 0:
+			entry: tuple[K, V] = self._queue.peekitem(0)
+			key = entry[0]
+			if key < first:
+				del self._queue[key]
+			else:
+				break
+
+	def clear(self):
+		self._queue.clear()
+		self.modcount += 1
+	
+	def add(self, key: K, value: V):
+		self._clean_up(key)
+		self._queue[key] = value
+		self.modcount += 1

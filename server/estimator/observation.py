87f0c5461adf40b76711478b9c32c4e7db18857c
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-11 18:23:40 -0400

    Tabs -> spaces

--- server/estimator/observation.py
+++ server/estimator/observation.py
@@ -4,44 +4,44 @@ from util.timestamp import Timestamp
 from typedef.geom import Pose3d
 
 class ObservationBase:
-    ts: Timestamp
-    is_control: bool = False
-    
-    def __init__(self, ts: Timestamp) -> None:
-        self.ts = ts
+	ts: Timestamp
+	is_control: bool = False
+	
+	def __init__(self, ts: Timestamp) -> None:
+		self.ts = ts
 
 class PoseOverrideObservation(ObservationBase):
-    "Pose override from Rio"
-    pose: Pose3d
-    is_control: bool = True
-    def __init__(self, ts: Timestamp, pose: Pose3d) -> None:
-        super().__init__(ts)
-        self.pose = pose
+	"Pose override from Rio"
+	pose: Pose3d
+	is_control: bool = True
+	def __init__(self, ts: Timestamp, pose: Pose3d) -> None:
+		super().__init__(ts)
+		self.pose = pose
 
 
 class OdometryObservation(ObservationBase):
-    "Rio odometry"
-    pass
+	"Rio odometry"
+	pass
 
 class SlamObservation(ObservationBase):
-    "Camera SLAM"
-    pass
+	"Camera SLAM"
+	pass
 
 class AprilTagObservation(ObservationBase):
-    "Camera AprilTag"
-    pass
+	"Camera AprilTag"
+	pass
 
 class ObjectDetectionObservation(ObservationBase):
-    "Object detection"
-    
+	"Object detection"
+	
 
 Observation: TypeAlias = Union[
-    PoseOverrideObservation,
-    OdometryObservation,
-    SlamObservation,
-    AprilTagObservation,
+	PoseOverrideObservation,
+	OdometryObservation,
+	SlamObservation,
+	AprilTagObservation,
 ]
 
 class PoseEstimator:
-    def observe(self, observation: Observation):
-        pass
+	def observe(self, observation: Observation):
+		pass
--- server/estimator/pose_gtsam.py
+++ server/estimator/pose_gtsam.py
@@ -8,24 +8,24 @@ from typedef.cfg import PoseEstimatorConfig
 from .observation import Observation, PoseEstimator, PoseOverrideObservation
 
 class _NonlinearISAM(Protocol):
-    def estimate(self): ...
+	def estimate(self): ...
 
 class NonLinearEstimator(PoseEstimator):
-    "Pose estimator that uses GTSAM as a backend"
-    
-    def __init__(self, config: PoseEstimatorConfig):
-        self.config = config
-        
-        self._isam = NonlinearISAM()
-        self._graph = NonlinearFactorGraph()
-        self._guesses = Values()
-        self._pose_id = 0
-        self._current_pose = Pose3d()
-    
-    def observe(self, observation: Observation):
-        if isinstance(observation, PoseOverrideObservation):
-            self._isam = NonlinearISAM()
-            self._graph.clear()
-            self._guesses.clear()
-            self._current_pose = observation.pose
-            
+	"Pose estimator that uses GTSAM as a backend"
+	
+	def __init__(self, config: PoseEstimatorConfig):
+		self.config = config
+		
+		self._isam = NonlinearISAM()
+		self._graph = NonlinearFactorGraph()
+		self._guesses = Values()
+		self._pose_id = 0
+		self._current_pose = Pose3d()
+	
+	def observe(self, observation: Observation):
+		if isinstance(observation, PoseOverrideObservation):
+			self._isam = NonlinearISAM()
+			self._graph.clear()
+			self._guesses.clear()
+			self._current_pose = observation.pose
+			

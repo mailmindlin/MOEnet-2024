6f796ceae87c888a825ea3dc2fcf8ed90a644383
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-11 18:23:58 -0400

    typing

--- server/estimator/pose/angles.py
+++ server/estimator/pose/angles.py
@@ -1,10 +1,10 @@
 import numpy as np
-def normalize_angle_positive(angle):
+def normalize_angle_positive(angle: float):
     """ Normalizes the angle to be 0 to 2*pi
         It takes and returns radians. """
     return angle % (2.0*np.pi)
 
-def normalize_angle(angle):
+def normalize_angle(angle: float):
     """ Normalizes the angle to be -pi to +pi
         It takes and returns radians."""
     a = normalize_angle_positive(angle)

9f15759a6e44ff05e243b8d77b005c54e460d45a
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-11 18:25:17 -0400

    More tabs

--- server/estimator/util/heap.py
+++ server/estimator/util/heap.py
@@ -3,26 +3,26 @@ import heapq
 
 T = TypeVar('T')
 class Heap(Generic[T]):
-    def __init__(self) -> None:
-        self.data: list[T] = list()
-    
-    def push(self, value: T):
-        heapq.heappush(self.data, value)
-    
-    def pop(self) -> T:
-        return heapq.heappop(self.data)
-    
-    def peek(self) -> T | None:
-        try:
-            return self.data[0]
-        except IndexError:
-            return None
-    
-    def __bool__(self):
-        return len(self.data) > 0
-    
-    def __len__(self):
-        return len(self.data)
+	def __init__(self) -> None:
+		self.data: list[T] = list()
+	
+	def push(self, value: T):
+		heapq.heappush(self.data, value)
+	
+	def pop(self) -> T:
+		return heapq.heappop(self.data)
+	
+	def peek(self) -> T | None:
+		try:
+			return self.data[0]
+		except IndexError:
+			return None
+	
+	def __bool__(self):
+		return len(self.data) > 0
+	
+	def __len__(self):
+		return len(self.data)
 
-    def clear(self):
-        self.data.clear()
+	def clear(self):
+		self.data.clear()
--- server/estimator/util/interpolated_test.py
+++ server/estimator/util/interpolated_test.py
@@ -2,79 +2,79 @@ from unittest import TestCase
 from .interpolated import InterpolatingBuffer
 
 def lerp(a: float, b: float, c: float):
-    if c < 0:
-        return a
-    if c > 1:
-        return b
-    return (b - a) * c + a
-    
+	if c < 0:
+		return a
+	if c > 1:
+		return b
+	return (b - a) * c + a
+	
 
 class TestInterpolate(TestCase):
-    def test_lerp(self):
-        buffer: InterpolatingBuffer[float, float, float] = InterpolatingBuffer(10, lerp)
-        self.assertEqual(len(buffer), 0)
-        self.assertIsNone(buffer.get(1))
+	def test_lerp(self):
+		buffer: InterpolatingBuffer[float, float, float] = InterpolatingBuffer(10, lerp)
+		self.assertEqual(len(buffer), 0)
+		self.assertIsNone(buffer.get(1))
 
-        buffer.add(1, 1)
-        self.assertEqual(len(buffer), 1)
-        self.assertEqual(buffer.get(1), 1)
-        self.assertEqual(buffer.get(2), 1)
-        self.assertEqual(buffer.get(0), 1)
+		buffer.add(1, 1)
+		self.assertEqual(len(buffer), 1)
+		self.assertEqual(buffer.get(1), 1)
+		self.assertEqual(buffer.get(2), 1)
+		self.assertEqual(buffer.get(0), 1)
 
-        buffer.add(2, 2)
-        self.assertEqual(len(buffer), 2)
-        self.assertEqual(buffer.get(0), 1)
-        self.assertEqual(buffer.get(1), 1)
-        self.assertEqual(buffer.get(1.5), 1.5)
-        self.assertEqual(buffer.get(2), 2)
-        self.assertEqual(buffer.get(3), 2)
+		buffer.add(2, 2)
+		self.assertEqual(len(buffer), 2)
+		self.assertEqual(buffer.get(0), 1)
+		self.assertEqual(buffer.get(1), 1)
+		self.assertEqual(buffer.get(1.5), 1.5)
+		self.assertEqual(buffer.get(2), 2)
+		self.assertEqual(buffer.get(3), 2)
 
-        buffer.add(3, 1)
-        self.assertEqual(len(buffer), 3)
-        self.assertEqual(buffer.get(0), 1)
-        self.assertEqual(buffer.get(1), 1)
-        self.assertEqual(buffer.get(1.5), 1.5)
-        self.assertEqual(buffer.get(2), 2)
-        self.assertEqual(buffer.get(2.5), 1.5)
-        self.assertEqual(buffer.get(3), 1)
+		buffer.add(3, 1)
+		self.assertEqual(len(buffer), 3)
+		self.assertEqual(buffer.get(0), 1)
+		self.assertEqual(buffer.get(1), 1)
+		self.assertEqual(buffer.get(1.5), 1.5)
+		self.assertEqual(buffer.get(2), 2)
+		self.assertEqual(buffer.get(2.5), 1.5)
+		self.assertEqual(buffer.get(3), 1)
 
-        # Test cleanup
-        buffer.add(15, 0)
-        self.assertEqual(len(buffer), 1)
-        
-    def test_tracking(self):
-        buffer: InterpolatingBuffer[float, float, float] = InterpolatingBuffer(10, lerp)
-        ks = [0,1,2,3]
-        ts = [buffer.track(k) for k in ks]
+		# Test cleanup
+		buffer.add(15, 0)
+		self.assertEqual(len(buffer), 1)
+		
+	def test_tracking(self):
+		buffer: InterpolatingBuffer[float, float, float] = InterpolatingBuffer(10, lerp)
+		ks = [0,1,2,3]
+		ts = [buffer.track(k) for k in ks]
 
-        self.assertEqual(len(buffer), 0)
-        for t in ts:
-            self.assertTrue(t.is_fresh)
-            self.assertFalse(t.is_static)
-            self.assertIsNone(t.current)
-        
-        buffer.add(1, 1)
-        self.assertEqual(len(buffer), 1)
-        for t in ts:
-            self.assertFalse(t.is_fresh)
-            self.assertIsNone(t.current)
-        for t in ts:
-            t.refresh()
-            self.assertEqual(t.current, 1)
-            self.assertTrue(t.is_fresh)
-        
-        buffer.add(3, 2)
-        self.assertEqual(len(buffer), 2)
-        # Don't auto-refresh
-        # Points 0, 1 don't change
-        for i,t in enumerate(ts[:2]):
-            self.assertTrue(t.is_fresh, f"Tracker {i} is fresh")
-            self.assertEqual(t.current, 1)
-        # Points 2, 3 change
-        for t in ts[2:]:
-            self.assertFalse(t.is_fresh)
-            self.assertEqual(t.current, 1)
-        # Refresh updates value
-        for k,t in zip(ks,ts):
-            t.refresh()
-            self.assertEqual(t.current, buffer.get(k))
+		self.assertEqual(len(buffer), 0)
+		for t in ts:
+			self.assertTrue(t.is_fresh)
+			self.assertFalse(t.is_static)
+			self.assertIsNone(t.current)
+		
+		buffer.add(1, 1)
+		self.assertEqual(len(buffer), 1)
+		for t in ts:
+			self.assertFalse(t.is_fresh)
+			self.assertIsNone(t.current)
+		for t in ts:
+			t.refresh()
+			self.assertEqual(t.current, 1)
+			self.assertTrue(t.is_fresh)
+		
+		buffer.add(3, 2)
+		self.assertEqual(len(buffer), 2)
+		# Don't auto-refresh
+		# Points 0, 1 don't change
+		for i,t in enumerate(ts[:2]):
+			self.assertTrue(t.is_fresh, f"Tracker {i} is fresh")
+			self.assertEqual(t.current, 1)
+		# Points 2, 3 change
+		for t in ts[2:]:
+			self.assertFalse(t.is_fresh)
+			self.assertEqual(t.current, 1)
+		# Refresh updates value
+		for k,t in zip(ks,ts):
+			t.refresh()
+			self.assertEqual(t.current, buffer.get(k))

e9091519dd5cbf11288e3fcb3866f536e8ca753b
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-15 15:16:48 -0400

    Better generics

--- server/util/timemap.py
+++ server/util/timemap.py
@@ -1,7 +1,8 @@
-from typing import TYPE_CHECKING, Sequence, Optional, TypeVar, Union, Generic
+from typing import TYPE_CHECKING, Sequence, Optional, Union, Self, Any, cast
 from abc import ABC, abstractmethod
 from functools import cached_property
 import operator, contextvars
+from dataclasses import dataclass
 
 from .decorators import classproperty
 
@@ -9,11 +10,7 @@ if TYPE_CHECKING:
 	from .clock import Clock, OffsetClock
 	from .timestamp import Timestamp
 
-CA = TypeVar('CA', bound='Clock')
-CB = TypeVar('CB', bound='Clock')
-C = TypeVar('C', bound='Clock')
-
-class TimeMapper(ABC, Generic[CA, CB]):
+class TimeMapper[CA: 'Clock', CB: 'Clock'](ABC):
 	"Identity time mapper"
 	clock_a: CA
 	clock_b: CB
@@ -61,7 +58,7 @@ class TimeMapper(ABC, Generic[CA, CB]):
 		"Invert"
 		return InverseTimeMapper(self)
 	
-	def __shr__(self, rhs: 'TimeMapper[CB, C]') -> 'TimeMapper[CA, C]':
+	def __shr__[C: 'Clock'](self, rhs: 'TimeMapper[CB, C]') -> 'TimeMapper[CA, C]':
 		"Chain TimeMappers"
 		if isinstance(rhs, TimeMapper):
 			assert self.clock_b == rhs.clock_a
@@ -69,26 +66,35 @@ class TimeMapper(ABC, Generic[CA, CB]):
 		return NotImplemented
 
 
-M = TypeVar('M', bound=TimeMapper)
+@dataclass
+class _TimeMapEntry:
+	mapper: TimeMapper
+	derived: bool
+	"Whether this entry is derived from another entry"
+
+
 class TimeMap:
 	@classproperty
 	def default(cls) -> 'TimeMap':
+		"Get default map"
 		return MAP_CTX.get()
 
 	def __init__(self, *mappers: Union['TimeMap', TimeMapper]):
 		self._prev_t = None
-		self.conversions: dict['Clock', dict['Clock', tuple[TimeMapper, bool]]] = dict()
+		self.conversions: dict['Clock', dict['Clock', _TimeMapEntry]] = dict()
 		for mapper in mappers:
 			if isinstance(mapper, TimeMap):
 				# Inherit mappings
-				for src, prev_src in mapper.conversions.items():
-					loc_src = self.conversions.setdefault(src, dict())
-					for dst, to_dst in prev_src.items():
-						loc_src[dst] = to_dst
+				for src_clock, prev_src in mapper.conversions.items():
+					loc_src = self.conversions.setdefault(src_clock, dict())
+					for dst_clock, entry in prev_src.items():
+						# self.register(entry.mapper, replace=True, derived=entry.derived)
+						loc_src[dst_clock] = entry
 			else:
-				self.register(mapper, replace=True, is_caching=False)
+				self.register(mapper, replace=True, derived=False)
 	
 	def __enter__(self):
+		"Context manager that sets this as the default map"
 		self._prev_t = MAP_CTX.set(self)
 		return self
 
@@ -97,16 +103,27 @@ class TimeMap:
 		MAP_CTX.reset(self._prev_t)
 		del self._prev_t
 	
-	def cached(self, mapper: M) -> M:
+	def cached[M: TimeMapper](self, mapper: M) -> M:
+		"Store `mapper` in cache"
 		self.register(mapper, False, True)
 		return mapper
 
-	def register(self, mapper: TimeMapper, replace: bool = True, is_caching: bool = False) -> bool:
+	def register(self, mapper: TimeMapper, replace: bool = True, derived: bool = False) -> bool:
+		"""
+		Register mapper in this map.
+		
+		Parameters
+		----------
+		:param mapper: Conversion to insert
+		:param replace: If there was a conversion already stored, should it be replaced?
+		:param derived: Is `mapper` a cached conversion?
+		"""
 		from_a = self.conversions.setdefault(mapper.clock_a, dict())
-		prev_a = from_a.get(mapper.clock_b, None)
+		prev_a = from_a.get(mapper.clock_b, None) # Previous mapping
+
 		did_replace = False
-		if replace or (prev_a is None) or (prev_a[1] and not is_caching):
-			from_a[mapper.clock_b] = (mapper, is_caching)
+		if replace or (prev_a is None) or (prev_a.derived and not derived):
+			from_a[mapper.clock_b] = _TimeMapEntry(mapper, derived)
 			did_replace = True
 		
 		# Skip for identity maps
@@ -115,17 +132,32 @@ class TimeMap:
 		
 		# Insert replaced
 		from_b = self.conversions.setdefault(mapper.clock_b, dict())
-		prev_b = from_b.get(mapper.clock_a, None)
-		# We are more selective, because this is always 'cached'
-		if (prev_b is None) or (prev_b[1] and (replace or (did_replace and prev_b[0] == -prev_a[0]))):
-			from_b[mapper.clock_a] = (-mapper, True)
+
+		# We are more selective, because this is always 'derived'
+		replace_inverse = None
+		if (prev_b := from_b.get(mapper.clock_a, None)) is not None:
+			if prev_b.derived:
+				if replace:
+					replace_inverse = True
+				elif did_replace:
+					# Check that prev_b was derived from prev_a
+					#TODO: is this right?
+					assert prev_a is not None
+					replace_inverse = (prev_b.mapper == -prev_a.mapper)
+		else:
+			# Doesn't exist, so let's add it
+			replace_inverse = True
+
+		if replace_inverse:
+			from_b[mapper.clock_a] = _TimeMapEntry(-mapper, True)
 		return did_replace
 	
-	def get_direct(self, src: CA, dst: CB) -> Optional[TimeMapper[CA, CB]]:
+	def get_direct[Csrc: 'Clock', Cdst: 'Clock'](self, src: Csrc, dst: Cdst) -> Optional[TimeMapper[Csrc, Cdst]]:
+		"Find direct conversion from `src` -> `dst`"
 		# Try forwards
 		if from_a := self.conversions.get(src, None):
 			if a_to_b := from_a.get(dst, None):
-				return a_to_b[0]
+				return a_to_b.mapper
 		
 		# Try identity
 		if src == dst:
@@ -134,13 +166,13 @@ class TimeMap:
 		# Try backwards
 		if from_b := self.conversions.get(dst, None):
 			if b_to_a := from_b.get(src, None):
-				return self.cached(-b_to_a[0])
+				return self.cached(-b_to_a.mapper)
 		# No direct path found
 		return None
 	
-	def neighbors(self, src: 'Clock') -> Sequence[TimeMapper]:
-		if from_src := self.conversions.get(src, dict()):
-			res = [value for value, _was_cached in from_src.values()]
+	def neighbors[Csrc: 'Clock'](self, src: Csrc) -> Sequence[TimeMapper[Csrc, Any]]:
+		if from_src := self.conversions.get(src, None):
+			res = [entry.mapper for entry in from_src.values()]
 			if src not in from_src.keys():
 				res.append(IdentityTimeMapper(src))
 			return res
@@ -148,13 +180,14 @@ class TimeMap:
 			# Always provide identity
 			return [IdentityTimeMapper(src)]
 
-	def get_conversion(self, src: 'Clock', dst: 'Clock') -> Optional[TimeMapper]:
+	def get_conversion[Csrc: 'Clock', Cdst: 'Clock'](self, src: Csrc, dst: Cdst) -> Optional[TimeMapper[Csrc, Cdst]]:
 		import heapq
 		prev: dict['Clock', tuple[int, TimeMapper]] = dict()
 		# prev[src] = (0, None)
 		queue: list[tuple[int,'Clock']] = [(0, src)]
 		if src == dst:
-			return self.cached(IdentityTimeMapper(src))
+			res = self.cached(IdentityTimeMapper(src))
+			return cast(TimeMapper[Csrc, Cdst], res)
 
 		def get_cost(node: 'Clock'):
 			try:
@@ -171,9 +204,9 @@ class TimeMap:
 				continue
 			
 			for neighbor in self.neighbors(node):
-				assert neighbor.clock_a == node
+				assert neighbor.clock_a == node, "neighbors() invariant"
 				n_cost = neighbor.conversion_cost
-				assert n_cost > 0
+				assert n_cost > 0, "TimeMapper.conversion_cost invariant"
 				n_dist = dist + n_cost
 				# print(" -> neighbor", neighbor, 'cost=', n_cost, 'dist=', n_dist, n_dist < get_cost(neighbor.clock_b), n_dist < get_cost(dst))
 				# Only consider this new path if it's better than any path we've already found (both src->neighbor and src->dst)
@@ -279,7 +312,7 @@ class ChainedTimeMapper(TimeMapper):
 	def conversion_cost(self) -> int:
 		return sum(step.conversion_cost for step in self.steps) + 1
 
-	def __eq__(self, other) -> bool:
+	def __eq__(self, other, /) -> bool:
 		if self is other:
 			return True
 		if isinstance(other, ChainedTimeMapper):
@@ -295,23 +328,23 @@ class ChainedTimeMapper(TimeMapper):
 		return NotImplemented
 
 
-class FixedOffsetMapper(TimeMapper):
+class FixedOffsetMapper[CA: 'Clock', CB: 'Clock'](TimeMapper[CA, CB]):
 	"""
 	Try to compute the offset between the monotonic clock and system time
 	We use this to convert timestamps to system time between processes
 	"""
 
-	@staticmethod
-	def computed(clock_a: 'Clock', clock_b: 'Clock') -> 'FixedOffsetMapper':
+	@classmethod
+	def computed(cls, clock_a: CA, clock_b: CB) -> Self:
 		# Compute offset once
 		ts_a_1 = clock_a.now_ns()
 		ts_b = clock_b.now_ns()
 		ts_a_2 = clock_a.now_ns()
 		ts_a = (ts_a_1 + ts_a_2) // 2
 		offset_ns = ts_b - ts_a
-		return FixedOffsetMapper(clock_a, clock_b, offset_ns)
+		return cls(clock_a, clock_b, offset_ns)
 
-	def __init__(self, clock_a: 'Clock', clock_b: 'Clock', offset_ns: int) -> None:
+	def __init__(self, clock_a: CA, clock_b: CB, offset_ns: int) -> None:
 		super().__init__(clock_a, clock_b)
 		self.offset_ns = offset_ns
 	
@@ -327,13 +360,13 @@ class FixedOffsetMapper(TimeMapper):
 			return (self.clock_a == other.clock_a) and (self.clock_b == other.clock_b) and (self.offset_ns == other.offset_ns)
 		return super().__eq__(other)
 
-class DynamicOffsetMapper(TimeMapper):
+class DynamicOffsetMapper[CA: 'Clock', CB: 'Clock'](TimeMapper[CA, CB]):
 	"""
 	Try to compute the offset between the monotonic clock and system time
 	We use this to convert timestamps to system time between processes
 	"""
 
-	def __init__(self, clock_a: 'Clock', clock_b: 'Clock') -> None:
+	def __init__(self, clock_a: CA, clock_b: CB) -> None:
 		super().__init__(clock_a, clock_b)
 	
 	@property
@@ -351,8 +384,8 @@ class DynamicOffsetMapper(TimeMapper):
 		return super().__eq__(other)
 
 
-class IdentityTimeMapper(TimeMapper):
-	def __init__(self, clock: 'Clock') -> None:
+class IdentityTimeMapper[C: 'Clock'](TimeMapper[C, C]):
+	def __init__(self, clock: C) -> None:
 		super().__init__(clock, clock)
 	
 	@property
@@ -377,10 +410,10 @@ class IdentityTimeMapper(TimeMapper):
 		return super().__eq__(other)
 
 
-class OffsetClockMapper(TimeMapper):
+class OffsetClockMapper[C: 'OffsetClock'](TimeMapper['Clock', C]):
 	"TimeMapper generated from an OffsetClock"
-	clock_b: 'OffsetClock'
-	def __init__(self, clock: 'OffsetClock') -> None:
+	# clock_b: 'OffsetClock'
+	def __init__(self, clock: C) -> None:
 		super().__init__(clock.base, clock)
 	
 	@property

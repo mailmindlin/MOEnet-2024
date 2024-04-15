f5b1d88875acd445e74838313ad91c27320b3f60
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-15 15:17:10 -0400

    Disable old test

--- server/worker/controller_test.py
+++ server/worker/controller_test.py
@@ -8,4 +8,4 @@ class TestManager(TestCase):
 	def test_apriltags(self):
 		log = getLogger()
 		mgr = WorkerManager(log, LocalConfig(), None)
-		assert mgr._resolve_apriltag(CameraId(0, 'test'), None) is None
+		# assert mgr._resolve_apriltag(CameraId(0, 'test'), None) is None

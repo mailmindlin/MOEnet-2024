2ebfbe8e85e03ec286d50909fe65c0668d51f89c
Liam Feehery <mailmindlin@users.noreply.github.com>
2024-04-15 15:15:53 -0400

    Doc

--- server/estimator/tf.py
+++ server/estimator/tf.py
@@ -1,15 +1,18 @@
-from typing import Literal, ClassVar, Protocol
+from typing import ClassVar, Protocol, Sequence
 from dataclasses import dataclass
 import enum
 
 from typedef.geom import Transform3d, Pose3d
-from util.timestamp import Timestamp
+from util.timestamp import Timestamp, Stamped
 from .util.cascade import Tracked, StaticValue
 
 class ReferenceFrameKind(enum.Enum):
 	FIELD = enum.auto()
+	"Absolute field frame"
 	ODOM = enum.auto()
+	"Robot odometry position"
 	ROBOT = enum.auto()
+	"Robot position"
 	CAMERA = enum.auto()
 	DETECTION = enum.auto()
 
@@ -40,7 +43,24 @@ ReferenceFrame.ROBOT = ReferenceFrame(ReferenceFrameKind.ROBOT)
 ReferenceFrame.ODOM = ReferenceFrame(ReferenceFrameKind.ODOM)
 
 class TfProvider(Protocol):
-	def track_tf(self, src: ReferenceFrame, dst: ReferenceFrame, timestamp: Timestamp | None = None) -> Tracked[Transform3d | None]:
+	"Data provider for [TfTracker]"
+	@property
+	def supported_conversions(self) -> Sequence[tuple[ReferenceFrame, ReferenceFrame]]:
+		"Enumerate supported conversions"
+		...
+
+	def track_tf(self, base: ReferenceFrame, target: ReferenceFrame, timestamp: Timestamp | None = None) -> Tracked[Stamped[Transform3d] | None]:
+		"""
+		Track transform
+		
+		:param base: Base frame
+		:param target: Target frame
+		:param timestamp: When to get transform. If `None`, get the latest transform.
+
+		Returns
+		-------
+		Stamped transform from src -> dst frame
+		"""
 		...
 
 
@@ -58,16 +78,16 @@ class TfTracker:
 		else:
 			raise ValueError()
 
-	def track_tf(self, src: ReferenceFrame, dst: ReferenceFrame, timestamp: Timestamp | None = None) -> Tracked[Transform3d | None]:
-		if src == dst:
+	def track_tf(self, base: ReferenceFrame, target: ReferenceFrame, timestamp: Timestamp | None = None) -> Tracked[Stamped[Transform3d] | None]:
+		if base == target:
 			# Identity
-			return StaticValue(Transform3d())
+			return StaticValue(Stamped(Transform3d(), Timestamp.invalid()))
 		
-		if src.kind == dst.kind:
+		if base.kind == target.kind:
 			raise ValueError()
 		
-		provider = self.get_provider(src.kind, dst.kind)
-		return provider.track_tf(src, dst, timestamp)
+		provider = self.get_provider(base.kind, target.kind)
+		return provider.track_tf(base, target, timestamp)
 	
 	def track_pose(self, dst: ReferenceFrame, timestamp: Timestamp | None = None) -> Tracked[Pose3d]:
 		"Special case where we track_tf relative to the field"
